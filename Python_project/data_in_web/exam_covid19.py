008d6b6e899afb875ec6239de3b309f434d006d4
cong <cong.pttc@gmail.com>
2021-09-04 19:53:19 +0700

    covid 19 uplate

--- Python_project/data_in_web/exam_covid19.py
+++ Python_project/data_in_web/exam_covid19.py
@@ -3,11 +3,19 @@ from collections import namedtuple
 import requests
 from lxml import html
 
-covid_data = namedtuple("covid_data", "cases deaths recovered")
-def covid_stats(url: str = "https://www.worldometers.info/coronavirus/") -> covid_data:
+fmt = """\nTổng Số Người Mắc COVID-19 : {} Người
+\nTỏng Số Người Tử Vong Do COVID-19 : {} Người
+\nTổng Số Người Đã Được Chữa Khỏi COVID-19 : {} Người"""
+
+url_vn = "https://www.worldometers.info/coronavirus/country/viet-nam/"
+url_world = "https://www.worldometers.info/coronavirus/"
+
+def covid_stats(url):
     xpath_str = '//div[@class = "maincounter-number"]/span/text()'
-    return covid_data(*html.fromstring(requests.get(url).content).xpath(xpath_str))
-fmt = """\nTotal COVID-19 cases in the world: {}
-\nTotal deaths due to COVID-19 in the world: {}
-\nTotal COVID-19 patients recovered in the world: {}"""
-print(fmt.format(*covid_stats())) 
+    x = html.fromstring(requests.get(url).content).xpath(xpath_str)
+    print(fmt.format(*x))
+
+print("Ở VIỆT NAM: ")
+covid_stats(url_vn)
+print("\nTRÊN THẾ GIỚI")
+covid_stats(url_world)
