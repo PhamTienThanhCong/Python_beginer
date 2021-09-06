fdb1cff630f378d206ae71015a7c6effe6b726e5
cong <cong.pttc@gmail.com>
2021-09-07 00:06:45 +0700

    lấy số đề từ web về máy

--- Python_project/data_in_web/tesst.py
+++ Python_project/data_in_web/tesst.py
@@ -6,7 +6,7 @@ from collections import namedtuple
 
 # Bước 1 kiểm tra xem nó cho phép không?
 
-url = 'https://xoso.com.vn/xsmb-01-01-2020.html'
+url = 'https://xoso.com.vn/xsmb-06-09-2021.html'
 
 htlm_web = requests.get(url)
 
@@ -15,9 +15,9 @@ doc =  html.fromstring(htlm_web.content)
 # print(htlm_web)
 
 # xpath_name = "//*[@id='table-body-scroll']"
-data = doc.xpath('//div[@id="kqngay_01012020_kq"]/table/tbody/tr[2]/td/span/text()')
-print('giải đặc biệt:',data[0])
+data = doc.xpath('//*[@id="kqngay_06092021_kq"]/table/tbody/tr[2]/td/span/text()')
+print("giải đặc biệt",data)
 for i in range(3,10):
-   data = doc.xpath('//div[@id="kqngay_01012020_kq"]/table/tbody/tr['+str(i)+']/td/span/text()')
+   data = doc.xpath('//*[@id="kqngay_06092021_kq"]/table/tbody/tr['+str(i)+']/td/span/text()')
    print('giải ' ,i-2 , ':',data[0:])
 
