f7ab4fa9d0ee277a79f83fa8812cc7ab2df7f607
cong <cong.pttc@gmail.com>
2021-09-10 10:24:27 +0700

    lỗi ở phần get data

---
+++ Python_project/data_in_web/So_So_mien_bac/gappic.py
@@ -0,0 +1,93 @@
+from re import search
+from tkinter import *
+from typing import Sized
+from handle_data import *
+from pull_data_in_web import *
+import time
+
+main = Tk()
+
+main.title("xổ số miền bắc")
+main.resizable(height=True,width=True)
+main.minsize(height=300,width=500)
+
+kq=StringVar()
+Ngay  = StringVar()
+Thang = StringVar()
+Nam   = StringVar()
+
+def data_1():
+    ngay =  str(Ngay.get())
+    thang = str(Thang.get())
+    nam = str(Nam.get())
+    return ngay,thang,nam
+
+def data_2():
+    today = date.today()
+    day   = today.strftime("%d")
+    month = today.strftime("%m")
+    year  = today.strftime("%Y")
+    return day,month,year
+
+def data_check():
+    a,b,c=data_1()
+    x,y,z=data_2()
+    if (a==x and b==y and c==z):
+        if (check_today==True):
+            text_print = "Ngày hoom nay"
+        else:
+            text_print = "Chưa Tới giờ "
+        Label(main,text=text_print).grid(row=9,column=0)
+    
+    data_train = get_url(x,y,z)
+    print_data(data_train)
+
+
+
+
+def getdata():
+    ngay,thang,nam=data_1()
+    text_print = "Ngày: "+str(ngay)+"-"+str(thang)+"-"+str(nam)
+    Label(main,text=text_print).grid(row=9,column=0)
+    
+def settoday():
+    day,month,year = data_2()
+    Ngay.set(day)
+    Thang.set(month)
+    Nam.set(year)
+    
+
+def search_data():
+    data_check()
+    
+    
+
+
+
+Label(main,text="KẾT QUẢ SỔ SỐ MIỀN BẮC",justify=CENTER).grid(row=0,columnspan=3)
+
+for i in range(0,len(name_data)):
+    Label(main,text=name_data[i]).grid(row=i+1,column=0)
+    colum=2
+    for j in range(0,len(data[i])):
+        data[i][j]=StringVar()
+        Entry(main,width=10,textvariable=data[i][j]).grid(row=i+1,column=colum)
+        colum+=1
+
+Label(main,text="Ngày: ").grid(row=9,column=0)
+
+B = Button(text="tra cứu",command=search_data)
+B.grid(row=11,column=6)
+
+B = Button(text=" to day ",command=settoday)
+B.grid(row=11,column=4)
+
+B = Button(text="set day",command=getdata)
+B.grid(row=11,column=5)
+
+Label(main,text="Nhập ngày tháng năm:  ").grid(row=11,column=0)
+Entry(main,width=5,font=("Tahoma",12),textvariable=Ngay).grid(row=11,column=1)
+Entry(main,width=5,font=("Tahoma",12),textvariable=Thang).grid(row=11,column=2)
+Entry(main,width=5,font=("Tahoma",12),textvariable=Nam).grid(row=11,column=3)
+
+main.mainloop()
--- Python_project/data_in_web/So_So_mien_bac/handle_data.py
+++ Python_project/data_in_web/So_So_mien_bac/handle_data.py
@@ -3,14 +3,21 @@ from tkinter.constants import FALSE
 from pull_data_in_web import *
 import time
 from datetime import date   
-from gappic import *
 
 data = [['87728'], ['88415'], ['19940 ', ' 44927'], ['26793 ', ' 17788 ', ' 33474 ', ' 52904 ', ' 88310 ', ' 77646'], ['1183 ', ' 9036 ', ' 7786 ', ' 1612'], ['8192 ', ' 9483 ', ' 3950 ', ' 4803 ', ' 7481 ', ' 1667'], ['792 ', ' 575 ', ' 369'], ['71 ', ' 02 ', ' 19 ', ' 65']]
 
 data_train = [['87728'], ['88415'], ['19940 ', ' 44927'], ['26793 ', ' 17788 ', ' 33474 ', ' 52904 ', ' 88310 ', ' 77646'], ['1183 ', ' 9036 ', ' 7786 ', ' 1612'], ['8192 ', ' 9483 ', ' 3950 ', ' 4803 ', ' 7481 ', ' 1667'], ['792 ', ' 575 ', ' 369'], ['71 ', ' 02 ', ' 19 ', ' 65']]
 name_data = ['Giải đặc biệt','Giải 1','Giải 2','Giải 3','Giải 4','Giải 5','Giải 6','Giải 7']
 
-def chane():
+def get_url(day,month,year):
+    url = 'https://xoso.com.vn/xsmb-'+day+'-'+month+'-'+year+'.html'
+    day = day+month+year
+    return get_data(url,day)
+
+data_train = get_url('09','09','2021')
+
+def print_data(data_train):
+    print(data_train)
     for i in range(0,len(data_train)):
         for j in range(0,len(data_train[i])):
             data[i][j].set(data_train[i][j])
@@ -28,20 +35,23 @@ def check_today():
     else: 
         return False
 
-def settoday():
-    today = date.today()
-    day   = today.strftime("%d")
-    month = today.strftime("%m")
-    year  = today.strftime("%Y")
-    return day,month,year
 
+
+# def settoday():
+#     today = date.today()
+#     day   = today.strftime("%d")
+#     month = today.strftime("%m")
+#     year  = today.strftime("%Y")
+#     if check_today==True:
+#         # Label(main,text="Ngay chuan").grid(row=9,column=0)
+#         print("done")
 
 
 # day,month,year = settoday()
-day,month,year = profit_calculator()
-
-url = 'https://xoso.com.vn/xsmb-'+day+'-'+month+'-'+year+'.html'
-day = day+month+year
+# day,month,year = settoday()
 
 
+
+# url = 'https://xoso.com.vn/xsmb-09-09-2021.html'
+# day = '09092021'
 
